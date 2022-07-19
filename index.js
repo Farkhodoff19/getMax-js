da2eaf46eaa0278217ed8b748288c4d865b4c9d0
Farkhodoff21 <farkhodoffcodes@gmail.com>
2022-07-19 22:00:33 +1400

    tamam

---
+++ index.js
@@ -0,0 +1,11 @@
+function getMax(a , b) {
+    return(a > b) ? a : b
+}
+let number = getMax(3,4)
+console.log(number);
+
+function isLandScape(width, height) {
+    return(width > height)
+}
+let widthHeight = isLandScape(20,10)
+console.log(widthHeight );
